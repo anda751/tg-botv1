e048016eadec7020b55cf9175c8ad44d5972fafc
anda <anda.jehuma@gmail.com>
2026-05-21 11:54:39 +0700

    fix: add ws transport for supabase realtime

--- strpi/src/services/supabase.ts
+++ strpi/src/services/supabase.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 import ws from 'ws';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY!;
+const SUPABASE_URL = process.env.SUPABASE_URL!;
+const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY, {
   realtime: {
     transport: ws as any,
   },
